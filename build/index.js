bf608c461c606b7aefc2f4688e0b0d123352ecb2
Gonzalo Umaña <65423749+Chalo15@users.noreply.github.com>
2024-04-09 16:00:47 -0600

    feat: update files

--- build/index.js
+++ build/index.js
@@ -3,7 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+const express_1 = __importDefault(require("express")); //ESModules
+const diaries_1 = __importDefault(require("./routes/diaries"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json()); //middleware who transform the req.body to json
 const PORT = 3000;
@@ -11,6 +12,7 @@ app.get('/ping', (_req, res) => {
     console.log('someone pinged here!!');
     res.send('pong');
 });
+app.use('/api/diaries', diaries_1.default);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
