59d332bed9f625a572584b24fe3cff9161505c6a
Gonzalo Umaña <65423749+Chalo15@users.noreply.github.com>
2024-04-09 15:31:41 -0600

    feat: add new type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,9 +1,10 @@
 export type Weather = "sunny" | "rainy" | "cloudy" | "windy" | "stormy";
 export type Visibility = "great" | "good" | "ok" | "poor";
-export interface DiatyEntry {
+export interface DiaryEntry {
   id: number;
   date: string;
   weather: Weather;
   visibility: Visibility;
   comment: string;
 }
+export type NonSensitiveInfoDiary = Omit<DiaryEntry, "comment">;
